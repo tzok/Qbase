e5eb6e7c78641387af5ff8f4dffb02e2115d9bc4
Paulina Pielacinska <paulina.pielacinska@student.put.poznan.pl>
2019-08-30 07:28:07 +0200

    Added new component

---
+++ RNAqbase/ClientApp/src/app/visualization/visualization.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-visualization',
+  templateUrl: './visualization.component.html',
+  styleUrls: ['./visualization.component.css']
+})
+export class VisualizationComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<VisualizationComponent>) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ RNAqbase/Models/TetradReference.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNAqbase.Models
+{
+    public class TetradReference : BaseEntity
+    {
+        public int Id { get; set; }
+        public int QuadruplexId { get; set; }
+        public string Sequence { get; set; }
+        public string OnzClass { get; set; }
+        public float Planarity { get; set; }
+        public float Twist { get; set; }
+        public float Rise { get; set; }
+
+
+
+    }
+}
