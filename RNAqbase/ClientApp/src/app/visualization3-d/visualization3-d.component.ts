691fc706397a0ef7471fd4a9cd462cc96071796e
Natalia Kraszewska <natkrasz5@gmail.com>
2021-04-10 17:17:46 +0200

    Add Coloring selection

--- RNAqbase/ClientApp/src/app/visualization3-d/visualization3-d.component.ts
+++ RNAqbase/ClientApp/src/app/visualization3-d/visualization3-d.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {runInThisContext} from "vm";
+import {Template} from "@angular/compiler/src/render3/r3_ast";
 
 
 @Component({
@@ -13,18 +14,32 @@ export class Visualization3DComponent implements OnInit {
   pdbId: string;
   url: string;
   liteMolPlugin;
-  type = 'Surface';
+  type = 'BallsAndSticks';
+  Coloring = ''
   t: any;
   plugin: any;
   Transformer: any;
   selectedValue: string;
+  style:any;
+  theme = LiteMol.Bootstrap.Visualization.Molecule.Default.Themes.filter(t => t.name === 'Element Symbol' )[0]
 
-  types: Type[] = [
+
+  types: Option[] = [
     {value: 'BallsAndSticks', viewValue: 'BallsAndSticks'},
     {value: 'Surface', viewValue: 'Surface'},
     {value: 'VDWBalls', viewValue: 'VDWBalls'}
   ];
 
+  coloring: Option[] = [
+    {value: 'Chain ID', viewValue: 'Chain ID'},
+    {value: 'Entity ID', viewValue: 'Entity ID'},
+    {value: 'Entity Type', viewValue: 'Entity Type'},
+    {value: 'Residue Name', viewValue: 'Residue Name'},
+    {value: 'Element Symbol', viewValue: 'Element Symbol'},
+    {value: 'Rainbow (Chain)', viewValue: 'Rainbow (Chain)'},
+    {value: 'Rainbow (Entity)', viewValue: 'Rainbow (Entity)'},
+    {value: 'Uniform Color', viewValue: 'Uniform Color'}
+  ];
 
   constructor(public dialogRef: MatDialogRef<Visualization3DComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
@@ -32,21 +47,27 @@ export class Visualization3DComponent implements OnInit {
   ngOnInit() {
     this.pdbId = this.data.pdbId;
     this.url = this.data.url;
-    console.log(this.url);
     this.show3DVisualization();
   }
 
   ChangeType(type: string){
-    this.addStructure(this.Transformer, this.plugin, type)
+    this.type = type
+    this.addStructure(this.Transformer, this.plugin, )
   }
 
+  ChangeColoring(coloring: string){
+   this.theme = LiteMol.Bootstrap.Visualization.Molecule.Default.Themes.filter(t => t.name === coloring )[0]
+    this.addStructure(this.Transformer, this.plugin)
+  }
 
-  addStructure(Transformer: any, plugin: any, type: string){
+  addStructure(Transformer: any, plugin: any){
+    plugin.clear();
     this.t = this.plugin.createTransform();
     this.t.add(plugin.root, Transformer.Data.Download, {
       url: this.url,
       type: 'String',
       id: this.pdbId,
+
     })
       .then(
         Transformer.Molecule.CreateFromData,
@@ -59,12 +80,18 @@ export class Visualization3DComponent implements OnInit {
         { ref: 'model' }
       )
       .then(Transformer.Molecule.CreateVisual, {
-        style: LiteMol.Bootstrap.Visualization.Molecule.Default.ForType.get(
-          type
-        )
+        style: {
+          ...LiteMol.Bootstrap.Visualization.Molecule.Default.ForType.get(this.type),
+          theme:
+            {
+              template: this.theme,
+              colors: this.theme.colors,
+              transparency: this.theme.transparency,
+              interactive: true
+            }
+        }
       });
     plugin.applyTransform(this.t);
-
   }
 
 
@@ -81,19 +108,16 @@ export class Visualization3DComponent implements OnInit {
     });
 
     this.Transformer = LiteMol.Bootstrap.Entity.Transformer;
-    this.addStructure(this.Transformer, this.plugin, 'BallsAndSticks')
+    this.addStructure(this.Transformer, this.plugin)
   }
 }
-
-
 
 interface DialogData {
   pdbId: string;
   url: string;
 }
 
-interface Type {
+interface Option {
   value: string;
   viewValue: string;
 }
-
