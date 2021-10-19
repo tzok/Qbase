1166fa7ce66448635e169183aa7065b129ed95f5
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2021-10-19 07:25:42 +0200

    Add information about the most recent update

--- RNAqbase/ClientApp/src/app/home/home.component.ts
+++ RNAqbase/ClientApp/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label, Color } from 'ng2-charts';
+import {ChartOptions, ChartType, ChartDataSets} from 'chart.js';
+import {Label, Color} from 'ng2-charts';
 import {ActivatedRoute} from "@angular/router";
 import {MatDialog} from "@angular/material/dialog";
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {formatDate} from '@angular/common';
 
 
 @Component({
@@ -16,7 +17,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   sub;
   count: componentsCount;
-  update: updateInfotmations;
+  update: updateInformations;
   info: info;
   email_added = false;
   model: any = {};
@@ -68,8 +69,27 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.sub = this.activatedRoute.paramMap.subscribe(params => {
-      this.http.get<updateInfotmations>(this.baseUrl + 'api/Statistics/GetUpdate').subscribe(result => {
+      this.http.get<updateInformations>(this.baseUrl + 'api/Statistics/GetUpdate').subscribe(result => {
         this.update = result;
+
+        /*
+         * If today is:
+         * - Sunday (0) -> update was 3 days ago
+         * - Monday (1) -> update was 4 days ago
+         * - Tuesday (2) -> update was 5 days ago
+         * - Wednesday (3) -> update was 6 days ago
+         * - Thursday (4) -> update was today
+         * - Friday (5) -> update was yesterday
+         * - Saturday (6) -> update was 2 days ago
+         */
+        let date: Date = new Date();
+        let day: number = date.getDay(); // 0=Sunday, 1=Monday, 2=Tuesday, 3=Wednesday, 4=Thursday, 5=Friday, 6=Saturday
+        if (day >= 4) {
+          date.setDate(date.getDate() - (day - 4));
+        } else {
+          date.setDate(date.getDate() - (day + 3));
+        }
+        this.update.recent = formatDate(date, 'yyyy-MM-dd', 'en_US');
 
         this.http.get<componentsCount>(this.baseUrl + 'api/Statistics/GetCount').subscribe(result => {
           this.count = result;
@@ -108,14 +128,14 @@ export class HomeComponent implements OnInit {
       this.http.get<info>(this.baseUrl + '' +
         'api/Newsletter/AddEmailToDatabase?email=' +
         '' + email_val.email).subscribe(result => {
-          this.email_added=true;
-          this.error_state=false;
+        this.email_added = true;
+        this.error_state = false;
 
-      }, error => this.error_state=true);
+      }, error => this.error_state = true);
     });
   }
 
-  wrong_email(){
+  wrong_email() {
     this.email_added = false;
   }
 }
@@ -127,12 +147,13 @@ interface componentsCount {
   structureCount: number;
 }
 
-interface updateInfotmations {
+interface updateInformations {
   addedHelixCount: number;
   addedQuadruplexCount: number;
   addedTetradCount: number;
   addedStructureCount: number;
   pdbRelease: string;
+  recent?: string;
 }
 
 interface info {
