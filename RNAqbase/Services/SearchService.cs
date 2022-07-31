17bf1623254ec509b5a2998cda2148f1aee6949a
Kamila Kremis <59363243+Anukia@users.noreply.github.com>
2022-07-31 19:28:20 +0200

    [ONQ-7] Code refactor

--- RNAqbase/Services/SearchService.cs
+++ RNAqbase/Services/SearchService.cs
@@ -10,6 +10,43 @@ namespace RNAqbase.Services
     public class SearchService
     {
         private readonly SearchRepository searchRepository;
+        private string query =
+@"SELECT
+MAX(q.id) AS Id,
+q.loop_class as LoopTopology,
+STRING_AGG(DISTINCT(qg.gba_quadruplex_class)::text,', ') AS TetradCombination,
+CONCAT(MAX(q.onzm), MAX(q.subtype)) AS OnzmClass,
+to_char(MAX(p.release_date)::date, 'YYYY-MM-DD') as PdbDeposition,
+MAX(p.identifier) AS PdbId,
+string_agg(DISTINCT(ion.name)::text, ', ') as Ion,
+string_agg(DISTINCT(ion.charge)::text, ', ') as Ion_charge,  
+MAX(p.assembly) AS AssemblyId,
+MAX(q_view.molecule) AS Molecule,
+MAX(p.experiment) AS Experiment,
+STRING_AGG(COALESCE((n1.short_name)||(n2.short_name)||(n3.short_name)||(n4.short_name), ''), '') AS Sequence,
+COUNT(DISTINCT SUBSTRING(t.onz::TEXT FROM 1 FOR 1)) AS TypeCount,
+COUNT(DISTINCT(t.id)) AS NumberOfTetrads,
+MAX(p.experiment) AS experiment,
+CASE
+		WHEN max(q_view.chains) = 1 THEN 'unimolecular'
+		WHEN max(q_view.chains) = 2 THEN  'bimolecular'
+		ELSE 'tetramolecular'
+	END 
+	as TypeOfStrands
+FROM QUADRUPLEX q
+JOIN QUADRUPLEX_GBA qg on qg.quadruplex_id = q.id
+JOIN TETRAD t ON q.id = t.quadruplex_id
+JOIN QUADRUPLEX_VIEW q_view ON q.id = q_view.id
+JOIN NUCLEOTIDE n1 ON t.nt1_id = n1.id
+JOIN NUCLEOTIDE n2 ON t.nt2_id = n2.id
+JOIN NUCLEOTIDE n3 ON t.nt3_id = n3.id
+JOIN NUCLEOTIDE n4 ON t.nt4_id = n4.id
+JOIN PDB p ON n1.pdb_id = p.id
+LEFT JOIN pdb_ion ON p.id = pdb_ion.pdb_id
+LEFT JOIN ion ON ion.id = pdb_ion.ion_id
+GROUP BY q.id
+HAVING COUNT(t.id) > 1
+";
 
         public SearchService(SearchRepository searchRepository)
         {
@@ -18,7 +55,7 @@ namespace RNAqbase.Services
 
         public string GetTest()
         {
-            return searchRepository.GetTest();
+            return query;
         }
     }
 
