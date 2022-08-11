2251c74a74fdfc8bdae5e03caff77fc410e28c3b
Kacper Matecki <kapi11067@wp.pl>
2022-08-11 23:03:45 +0200

    ONQ-14 added NoTetradsFilter

---
+++ RNAqbase/Models/Search/NoTetradsFilter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNAqbase.Models.Search
+{
+    public class NoTetradsFilter : Filter
+    {
+        NoTetradsFilter()
+        {
+            Conditions.Add(new Condition(">", "1"));
+            Conditions.Add(new Condition("<", "2"));
+            Conditions.Add(new Condition("=", "3"));
+            Conditions.Add(new Condition("{}", "2,4"));
+        }
+        public new readonly string FieldInSQL = "COUNT(DISTINCT(t.id))";
+        public override List<Condition> Conditions { get; set ; } = new List<Condition>();
+
+        public override string JoinConditions()
+        {
+            if (Conditions.Count == 0)
+            {
+                return "";
+            }
+            string query = "(";
+            for (int i = 0; i < Conditions.Count; i++)
+            {
+                if (Conditions[i].Operator == ">")
+                {
+                    query += $"({FieldInSQL} > {Conditions[i].Value})";
+                }
+                else if(Conditions[i].Operator == "<")
+                {
+                    query += $"({FieldInSQL} < {Conditions[i].Value})";
+                }
+                else if (Conditions[i].Operator == "=")
+                {
+                    query += $"({FieldInSQL} = {Conditions[i].Value})";
+                }
+                else if (Conditions[i].Operator == "{}")
+                {
+                    query += $"({FieldInSQL} BETWEEN {Conditions[i].Value.Split(",")[0]} AND {Conditions[i].Value.Split(",")[1]})";
+                }
+                if (i != Conditions.Count - 1)
+                {
+                    query += " OR ";
+                }
+            }
+            return query + ")";
+        }
+    }
+}
