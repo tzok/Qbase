df057c0f691129371476ed530befae419c175c1f
Kamila Kremis <59363243+Anukia@users.noreply.github.com>
2022-08-17 22:34:13 +0200

    [ONQ-55] Added TypeFilter class.

---
+++ RNAqbase/Models/Search/TypeFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNAqbase.Models.Search
+{
+    public class TypeFilter : Filter
+    {
+        public new readonly string FieldInSQL = "?";
+        public override List<Condition> Conditions { get; set; } = new List<Condition>();
+
+        public TypeFilter()
+        {
+
+        }
+
+        public override string JoinConditions()
+        {
+            if (Conditions.Count == 0 || Conditions.Where(x => x.Value == "any").ToList().Any())  //TODO wydzielić do klasy abstrakcyjnej? wtedy join by miało walidację i właściwy join
+            {
+                return "";
+            }
+
+            string query = $"({FieldInSQL} IN ('{Conditions[0].Value}'";
+            for (int i = 1; i < Conditions.Count; i++)
+            {
+                query += $", '{Conditions[i].Value}'";
+            }
+
+            return query + "))";
+        }
+    }
+}
