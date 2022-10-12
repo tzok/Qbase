132380d93faa5109c46846065ee39ff8ddd52ce8
Filip <fil.kokosza@gmail.com>
2022-10-12 14:31:48 +0200

    Added new row components

---
+++ RNAqbase/ClientApp/src/app/row-cond-addable/row-cond-addable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-row-cond-addable',
+  templateUrl: './row-cond-addable.component.html',
+  styleUrls: ['./row-cond-addable.component.css']
+})
+export class RowCondAddableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ RNAqbase/ClientApp/src/app/row-cond-nonaddable/row-cond-nonaddable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-row-cond-nonaddable',
+  templateUrl: './row-cond-nonaddable.component.html',
+  styleUrls: ['./row-cond-nonaddable.component.css']
+})
+export class RowCondNonaddableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
