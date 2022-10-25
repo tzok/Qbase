2d9f2ce3c4e229dd6349e64b6d3f66b0985a7e4e
Filip <fil.kokosza@gmail.com>
2022-10-25 18:06:17 +0200

    ONQ-62 Added new row-addable component

--- RNAqbase/ClientApp/src/app/row-cond-addable/row-cond-addable.component.ts
+++ RNAqbase/ClientApp/src/app/row-cond-addable/row-cond-addable.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RowElements } from '../row-elements';
 
 @Component({
   selector: 'app-row-cond-addable',
   templateUrl: './row-cond-addable.component.html',
   styleUrls: ['./row-cond-addable.component.css']
 })
-export class RowCondAddableComponent implements OnInit {
 
-  constructor() { }
+export class RowCondAddableComponent implements OnInit {
+  rowData: Array<RowElements>;
+  @Input() rowName: string;
+  @Input() rowElements: Array<RowElements>;
 
   ngOnInit() {
+    this.rowData = this.rowElements;
   }
 
 }
