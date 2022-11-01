9feb480bf1aecace1f891f2279e5aafdb9cc4afb
Filip <fil.kokosza@gmail.com>
2022-11-01 11:55:10 +0100

    ONQ - 63 Logic improved in condition component

--- RNAqbase/ClientApp/src/app/cond-clickable-cell/cond-clickable-cell.component.ts
+++ RNAqbase/ClientApp/src/app/cond-clickable-cell/cond-clickable-cell.component.ts
@@ -15,38 +15,35 @@ export class CondClickableCellComponent {
   isClicked: boolean;
 
   ngOnChanges(changes: SimpleChanges) {
-   // console.log(this.content);
-    //console.log(this.eventReceiver);
     if (changes['eventReceiver']) {
       this.eventRecv(this.eventReceiver);
-      console.log("condname: " + this.content);
-      console.log("change in " + this.attributeName);
-      console.log(this.eventReceiver);
     }
   }
 
   ngOnInit() {
     this.isClicked = false;
     if (this.content == 'any') {
-      console.log("INIT")
       this.clickEvent();
     }
   }
 
   eventRecv(pckt: RowCommPckt) {
-      //radio buttons situation, clicking other than any unClick any
     if (pckt.clickInvoker != '') {
-      if ((pckt.eventReceiver == 'any') && (this.content == 'any')) {
-        this.isClicked = false;
+      if (pckt.clickInvoker == 'row') {
+        if (pckt.eventReceiver == this.content) {
+          this.isClicked = true;
+        }
       }
-      //clicking 'any' behaves as radio button (unClick all buttons in row)
-      else if ((pckt.clickInvoker == 'any') && (this.content != 'any')) {
-        this.isClicked = false;
-      }
-      //radio buttons 
-      else if ((pckt.clickInvoker != this.content)&&(pckt.typeOfRow == 'radioSelect')) {
-        this.isClicked = false;
-        //change to invoking clickEvent() so that row will update its clicked cond table
+      else {
+        if ((pckt.eventReceiver == 'any') && (this.content == 'any')) {
+          this.isClicked = false;
+        }
+        else if ((pckt.clickInvoker == 'any') && (this.content != 'any')) {
+          this.isClicked = false;
+        }
+        else if ((pckt.clickInvoker != this.content) && (pckt.typeOfRow == 'radioSelect')) {
+          this.isClicked = false;
+        }
       }
     }
   }
@@ -54,20 +51,15 @@ export class CondClickableCellComponent {
   clickEvent() {
     if (this.isClicked)
     {
-      console.log("CLICKED: " + this.content);
-      //var pckt: CondCommPckt;
-      //pckt.clickInvoker = this.elementName;
       this.isClicked = false;
-      //this.clicked.emit(pckt);
+      const pckt = <CondCommPckt>{clickInvoker: this.content, clicked: false};
+      this.clicked.emit(pckt);
     }
     else
     {
-      console.log('childe clicked');
-      const pckt = <CondCommPckt> {clickInvoker: this.content, clicked: true};
-      //add clicked
-      console.log(pckt);
-      this.clicked.emit(pckt);
       this.isClicked = true;
+      const pckt = <CondCommPckt> {clickInvoker: this.content, clicked: true};
+      this.clicked.emit(pckt);
     }
   }
 }
