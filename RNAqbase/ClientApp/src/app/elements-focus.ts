e0b1597bf8e938265c6765985da37f8604beb5f9
Filip <fil.kokosza@gmail.com>
2022-11-01 11:55:53 +0100

    ONQ-63 Row component logic improved

---
+++ RNAqbase/ClientApp/src/app/elements-focus.ts
@@ -0,0 +1,3 @@
+export interface ElementsFocus {
+  [elemName: string]: boolean;
+}
--- RNAqbase/ClientApp/src/app/row-cond-nonaddable/row-cond-nonaddable.component.ts
+++ RNAqbase/ClientApp/src/app/row-cond-nonaddable/row-cond-nonaddable.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CondCommPckt } from '../cond-comm-pckt';
+import { ElementsFocus } from '../elements-focus';
 import { RowCommPckt } from '../row-comm-pckt';
 import { RowElements } from '../row-elements';
 
@@ -13,25 +14,60 @@ export class RowCondNonaddableComponent implements OnInit {
   @Input() rowName: string;
   @Input() rowType: string;
   @Input() rowElements: RowElements;
-  msg = <RowCommPckt>{clickInvoker: '', eventReceiver: ''};
+  msg = <RowCommPckt>{ clickInvoker: '', eventReceiver: '' };
+  rowElementsStatus: ElementsFocus = {};
 
   respondClickEvent(childPckt: CondCommPckt) {
-    console.log('parent: ' + this.rowName+ ' ' + childPckt.clickInvoker);
-    if (childPckt.clickInvoker == 'any') {
-      this.msg = { clickInvoker: childPckt.clickInvoker, eventReceiver: '' , typeOfRow: this.rowType};
-    }
-    else if (childPckt.clickInvoker != '') {
-      if (this.rowType == 'radioSelect') {
+    if (childPckt.clicked) {
+      if (childPckt.clickInvoker == 'any') {
+        this.rowElementsStatus[childPckt.clickInvoker] = childPckt.clicked;
+        this.unclickAll(childPckt.clickInvoker);
         this.msg = { clickInvoker: childPckt.clickInvoker, eventReceiver: '', typeOfRow: this.rowType };
       }
-      if ((this.rowType == 'multiSelect')&&(childPckt.clickInvoker != 'any')) {
-        this.msg = { clickInvoker: childPckt.clickInvoker, eventReceiver: 'any', typeOfRow: this.rowType };
+      else if (childPckt.clickInvoker != '') {
+        if (this.rowType == 'radioSelect') {
+          this.rowElementsStatus[childPckt.clickInvoker] = childPckt.clicked;
+          this.unclickAll(childPckt.clickInvoker);
+          this.msg = { clickInvoker: childPckt.clickInvoker, eventReceiver: '', typeOfRow: this.rowType };
+        }
+        if ((this.rowType == 'multiSelect') && (childPckt.clickInvoker != 'any')) {
+          this.rowElementsStatus[childPckt.clickInvoker] = childPckt.clicked;
+          this.rowElementsStatus['any'] = false;
+          this.msg = { clickInvoker: childPckt.clickInvoker, eventReceiver: 'any', typeOfRow: this.rowType };
+        }
       }
     }
+    else {
+      this.rowElementsStatus[childPckt.clickInvoker] = childPckt.clicked;
+      if (!this.checkIfAnyClicked()) {
+        this.msg = { clickInvoker: 'row', eventReceiver: 'any', typeOfRow: this.rowType };
+        this.rowElementsStatus['any'] = true;
+      }
+    }
+    console.log(this.rowElementsStatus);
   }
 
   ngOnInit() {
     this.rowData = this.rowElements;
+    for (let i of this.rowData.conditions) {
+        this.rowElementsStatus[i.condition] = false;
+    }
+  }
+
+  checkIfAnyClicked() {
+    let i = false;
+    for (let key in this.rowElementsStatus) {
+        i = i || this.rowElementsStatus[key];
+    }
+    return i;
+  }
+
+  unclickAll(elem: string) {
+    for (let key in this.rowElementsStatus) {
+      if (key != elem) {
+        this.rowElementsStatus[key] = false;
+      }
+    }
   }
 
 }
