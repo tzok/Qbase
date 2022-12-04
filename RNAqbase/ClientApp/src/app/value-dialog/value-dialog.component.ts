bf973e5e47c8c2e7be6b134357133590ae34a37d
Filip <fil.kokosza@gmail.com>
2022-12-04 16:43:24 +0100

    ONQ-53 Updated val dialog with logic

--- RNAqbase/ClientApp/src/app/value-dialog/value-dialog.component.ts
+++ RNAqbase/ClientApp/src/app/value-dialog/value-dialog.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { DialogData } from '../dialog-data';
 
 @Component({
   selector: 'app-value-dialog',
   templateUrl: './value-dialog.component.html',
   styleUrls: ['./value-dialog.component.css']
 })
-export class ValueDialogComponent implements OnInit {
+export class ValueDialogComponent {
+  
+  constructor(
+    public dialogRef: MatDialogRef<ValueDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
-
 }
