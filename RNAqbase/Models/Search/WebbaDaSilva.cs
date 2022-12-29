81173abb14e5fab3866dceaeca0b06a123ab112e
Anukia <59363243+Anukia@users.noreply.github.com>
2022-12-29 20:04:21 +0100

    Onq 17 jako naukowiec chce miec mozliwosc filtrowania obiektow poprzez pole webba da silva classification aby zawezic wyniki wyszukiwania (#40)
    * [ONQ-17] Added WebbaDaSilva filter draft
    
    * [ONQ-17] Added SQL field name

---
+++ RNAqbase/Models/Search/WebbaDaSilva.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNAqbase.Models.Search
+{
+    public class WebbaDaSilva : Filter
+    {
+        public WebbaDaSilva()
+        {
+            FieldInSQL = "loop_progression";
+        }
+
+        public override List<Condition> Conditions { get; set; } = new List<Condition>();
+
+        public override string JoinConditions()
+        {
+            if (Conditions.Count == 0 || Conditions.Where(x => x.Value == "any").ToList().Any())
+            {
+                return "";
+            }
+            string query = "(";
+            for (int i = 0; i < Conditions.Count; i++)
+            {
+                query += $"({FieldInSQL} LIKE '{Conditions[i].Value}%')";
+                if (i != Conditions.Count - 1)
+                {
+                    query += " OR ";
+                }
+            }
+
+            return query + ")";
+        }
+    }
+}
