8d0a63f1ef98641feff69856c4c710600ac5a0ba
Kacper Matecki <kapi11067@wp.pl>
2023-01-03 19:46:20 +0100

    WIP

--- RNAqbase/Models/Search/SequenceFilter.cs
+++ RNAqbase/Models/Search/SequenceFilter.cs
@@ -10,7 +10,7 @@ namespace RNAqbase.Models.Search
     {
         public SequenceFilter()
         {
-            FieldInSQL = "STRING_AGG(COALESCE((n1.short_name)||(n2.short_name)||(n3.short_name)||(n4.short_name), ''), '')";
+            FieldInSQL = "";
             joinType = JoinType.Having;
         }
         public override List<Condition> Conditions { get; set; } = new List<Condition>();
@@ -21,17 +21,81 @@ namespace RNAqbase.Models.Search
             {
                 return "";
             }
-            StringBuilder querySB = new StringBuilder($"({FieldInSQL} ");
-            if (Conditions.Where(x => x.Value == "includes").ToList().Any())
-            {
-
+            //StringBuilder querySB = new StringBuilder($"({FieldInSQL} ");
+            for (int i = 0; i < Conditions.Count; i++) {
+                if (Conditions[i].Operator == "begins") //na figmie żeby usunąć
+                {
+                    return "";
+                }
+                else if (Conditions[i].Operator == "includes")
+                {
+                    joinType = JoinType.Where;
+                    FieldInSQL = "tu dać tetrady w quadruplexie";
+                    string[] permutations = GetPermutations("CGTA").ToArray();
+                    IEnumerable<string> distinctPermutations = permutations.Distinct();
+                    foreach (string permutation in distinctPermutations)
+                    {
+                        Console.WriteLine(permutation);
+                    }
+                }
+                else if (Conditions[i].Operator == "5'->3'")
+                {
+                    joinType = JoinType.Where;
+                    FieldInSQL = "tu dać sekwencję quadruplexu";
+                    StringBuilder querySB = new StringBuilder($"({FieldInSQL} ~ '^[^-]+$')");
+                    return querySB.ToString();
+                }
+                else if (Conditions[i].Operator == "IUPAC")
+                {
+                    joinType = JoinType.Where;
+                    FieldInSQL = "tu dać sekwencję quadruplexu";
+                    var codes = new Dictionary<string, string>(){
+                    {"A", "A"},
+                    {"C", "C"},
+                    {"G", "G"},
+                    {"T", "T"},
+                    {"U", "U"},
+                    {"R", "AG"},
+                    {"Y", "CTU"},
+                    {"K", "GTU"},
+                    {"M", "AC"},
+                    {"S", "CG"},
+                    {"W", "ATU"},
+                    {"B", "CGTU"},
+                    {"D", "AGTU"},
+                    {"H", "ACTU"},
+                    {"V", "ACG"},
+                    {"N", "ACGTU"},
+                    {"-", "-"}
+                };
+                    StringBuilder querySB = new StringBuilder($"({FieldInSQL} ~ '");
+                    foreach (var letter in Conditions[i].Value)
+                    {
+                        querySB.Append($"[{codes[letter.ToString()]}]");
+                    }
+                    return querySB.ToString() + "')";
+                }
             }
-            else if (Conditions.Where(x => x.Value == "3'->5'").ToList().Any())
+            return "";
+        }
+        public static IEnumerable<string> GetPermutations(string input)
+        {
+            if (input.Length == 1)
             {
-
+                yield return input;
             }
-
-            return null;
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char c = input[i];
+                    string remaining = input.Substring(0, i) + input.Substring(i + 1);
+                    foreach (string permutation in GetPermutations(remaining))
+                    {
+                        yield return c + permutation;
+                    }
+                }
+            }
         }
     }
 }
