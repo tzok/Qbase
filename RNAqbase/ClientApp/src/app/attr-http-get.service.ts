ec8f546bf8bff7e576345e6f605a3a9e5fbeea37
Filip <fil.kokosza@gmail.com>
2023-01-07 00:55:22 +0100

    ONQ-88 Created access functions for endpoints

--- RNAqbase/ClientApp/src/app/attr-http-get.service.ts
+++ RNAqbase/ClientApp/src/app/attr-http-get.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,40 @@ import { Injectable } from '@angular/core';
 })
 export class AttrHttpGetService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(attrID: string) {
+    switch (attrID) {
+      case 'expMethod':
+        return this.getExperimentalMethod();
+      case 'molType':
+        return this.getMoleculeType();
+      case 'onzClass':
+        return this.getONZ();
+      case 'webbaDaSilva':
+        return this.getWebbaDaSilva();
+      case 'ions':
+        return this.getIons();
+    }
+  }
+
+  getExperimentalMethod() {
+    return this.http.get('api/Search/GetExperimentalMethod');
+  }
+
+  getONZ() {
+    return this.http.get('api/Search/GetONZ');
+  }
+
+  getIons() {
+    return this.http.get('api/Search/GetIons');
+  }
+
+  getMoleculeType() {
+    return this.http.get('api/Search/GetMoleculeType');
+  }
+
+  getWebbaDaSilva() {
+    return this.http.get('api/Search/GetWebbaDaSilva');
+  }
 }
