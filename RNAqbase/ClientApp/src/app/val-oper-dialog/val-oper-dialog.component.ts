452850049e57692c4313e79e4cab8b1f3cfc14e0
Filip <fil.kokosza@gmail.com>
2023-01-07 23:03:33 +0100

    ONQ-88 Updated logic of val-oper dialog

--- RNAqbase/ClientApp/src/app/val-oper-dialog/val-oper-dialog.component.ts
+++ RNAqbase/ClientApp/src/app/val-oper-dialog/val-oper-dialog.component.ts
@@ -8,12 +8,20 @@ import { DialogData } from '../dialog-data';
   styleUrls: ['./val-oper-dialog.component.css']
 })
 export class ValOperDialogComponent {
+  value: string;
+  operator: string;
+
   constructor(
     public dialogRef: MatDialogRef<ValOperDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   onNoClick(): void {
+    console.log(this.data.conditions);
     this.dialogRef.close(false);
   }
 
+  onAddClick(): void {
+    this.data.conditions.push({ value: this.value, operator: this.operator });
+    this.dialogRef.close(this.data);
+  }
 }
