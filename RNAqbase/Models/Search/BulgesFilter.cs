b4354cb574a533871321c8f592e6633371a88ca9
Dominik Łukasiewicz <dominikluk2@gmail.com>
2023-01-27 09:08:41 +0100

    fixed bulges filter

--- RNAqbase/Models/Search/BulgesFilter.cs
+++ RNAqbase/Models/Search/BulgesFilter.cs
@@ -16,13 +16,14 @@ namespace RNAqbase.Models.Search
 
         public override string Join()
         {
-            StringBuilder querySB = new StringBuilder(@"(SELECT COUNT(*)
+            StringBuilder querySB = new StringBuilder(@"(SELECT COUNT(count) FROM (
+SELECT COUNT(*) as count
 FROM quadruplex q2
 JOIN loop l on q2.id = l.quadruplex_id
 JOIN loop_nucleotide ln on l.id = ln.loop_id
 JOIN nucleotide n on ln.nucleotide_id = n.id
 WHERE q2.id = q.id
-GROUP BY l.id) ");
+GROUP BY l.id) bulges_select) ");
             if (Conditions[0].Value == "with bulges")
             {
                 querySB.Append("!= ");
@@ -31,8 +32,7 @@ GROUP BY l.id) ");
             {
                 querySB.Append("= ");
             }
-            querySB.Append(@"(SELECT COUNT(*)
-FROM REGEXP_MATCHES((REGEXP_SPLIT_TO_ARRAY(q.dot_bracket, '[.]*\\n[.]*'))[2], '(?<![.-])([.]+)(?![.-])', 'g'))");
+            querySB.Append(@"count_bulges(q.dot_bracket)");
             return querySB.ToString();
         }
     }
